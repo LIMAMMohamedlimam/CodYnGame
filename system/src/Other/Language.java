11b410d90a451be6f4306e18c7ee3509e9047237
LIMAMMohmedlimam <medlimamuia@gmail.com>
2024-05-08 02:01:33 +0200

    creating base code for all classes

---
+++ system/src/Other/Language.java
@@ -0,0 +1,11 @@
+package Other;
+
+public class Language {
+    private String name;
+    private String defaultCode;
+
+    public Language(String name, String defaultCode) {
+        this.name = name;
+        this.defaultCode = defaultCode;
+    }
+}
