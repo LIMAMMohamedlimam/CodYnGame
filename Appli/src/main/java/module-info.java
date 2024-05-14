4b1686edf110e2d1df91df370e7b2a44543ca493
axellllllllllllllllll <malherbeax@cy-tech.fr>
2024-05-14 23:41:38 +0200

    a

--- Appli/src/main/java/module-info.java
+++ Appli/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module main.appli {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.jetbrains.annotations;
 
     opens main.appli to javafx.fxml;
     exports main.appli;
