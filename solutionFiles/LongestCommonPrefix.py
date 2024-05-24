410102381dbcdad1c159529ff06822aaf41b5686
Rayane93s <162728083+Rayane93s@users.noreply.github.com>
2024-05-24 18:05:40 +0200

    Update LongestCommonPrefix.py

--- solutionFiles/LongestCommonPrefix.py
+++ solutionFiles/LongestCommonPrefix.py
@@ -2,12 +2,17 @@ def longestCommonPrefix(strs):
     if not strs:
         return ""
 
+   # Initialiser le préfixe avec la première chaîne du tableau
     prefix = strs[0]
-
-    for i in range(1, len(strs)):
-        while strs[i].startswith(prefix) == False:
+    
+    # Parcourir chaque chaîne dans le tableau à partir de la seconde
+    for s in strs[1:]:
+        # Réduire le préfixe jusqu'à ce qu'il soit commun avec s
+        while not s.startswith(prefix):
+            # Raccourcir le préfixe de un caractère à la fois
             prefix = prefix[:-1]
-            if len(prefix) == 0:
+            # Si le préfixe est vidé, aucun préfixe commun n'existe
+            if not prefix:
                 return ""
-
+    
     return prefix
