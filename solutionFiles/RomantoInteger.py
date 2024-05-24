c5c1a54c4d961fa8893ab2ecefdf33f3046a3e26
Rayane93s <162728083+Rayane93s@users.noreply.github.com>
2024-05-24 18:09:28 +0200

    Update RomantoInteger.py

--- solutionFiles/RomantoInteger.py
+++ solutionFiles/RomantoInteger.py
@@ -1,12 +1,27 @@
 def romanToInt(s: str) -> int:
+    # Dictionnaire des valeurs des chiffres romains
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    
+    # Initialisation de la valeur précédente à 0 pour commencer les comparaisons
     prev_value = 0
+    
+    # Initialisation du total à 0 pour accumuler le résultat
     total = 0
-    for i in s[::-1]: # reverse the string
+    
+    # Parcourir la chaîne de caractères en ordre inversé
+    for i in s[::-1]:  # La chaîne est inversée pour faciliter le traitement des soustractions
+        # Récupération de la valeur courante du caractère
         current_value = roman_dict[i]
+        
+        # Si la valeur précédente est supérieure à la valeur courante, cela signifie une soustraction
         if prev_value > current_value:
             total -= current_value
         else:
+            # Sinon, ajouter normalement la valeur courante au total
             total += current_value
+        
+        # Mise à jour de la valeur précédente pour le prochain tour de boucle
         prev_value = current_value
+    
+    # Retourner la valeur totale convertie
     return total
