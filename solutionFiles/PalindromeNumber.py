e159d8cca04fd415a55feac1614f3e8edb234a56
axellllllllllllllllll <malherbeax@cy-tech.fr>
2024-05-26 18:30:31 +0200

    a

--- solutionFiles/PalindromeNumber.py
+++ solutionFiles/PalindromeNumber.py
@@ -1,7 +1,5 @@
 def is_palindrome(x):
-    # Les nombres négatifs ne sont pas des palindromes
-    if x < 0:
-        return False
+
 
     # Convertir le nombre en une chaîne de caractères
     str_x = str(x)
@@ -9,15 +7,10 @@ def is_palindrome(x):
     # Vérifier si la chaîne est égale à son renversement
     return str_x == str_x[::-1]
 
-def main():
-    # Entrée de l'utilisateur : un nombre entier
-    num = int(input("Entrez un nombre entier : "))
-
-    # Appel de la fonction pour vérifier si le nombre est un palindrome
-    if is_palindrome(num):
-        print("Le nombre", num, "est un palindrome.")
-    else:
-        print("Le nombre", num, "n'est pas un palindrome.")
+def main(generatedData):
+    x = generatedData["x"]
+    result = is_palindrome(x)
+    print(result)
 
 if __name__ == "__main__":
     main()
