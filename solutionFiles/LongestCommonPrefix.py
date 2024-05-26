48dc2fdb69fa6ced264f45b7d50132d18cd5b6b0
axellllllllllllllllll <malherbeax@cy-tech.fr>
2024-05-26 18:33:38 +0200

    a

--- solutionFiles/LongestCommonPrefix.py
+++ solutionFiles/LongestCommonPrefix.py
@@ -2,30 +2,22 @@ def longest_common_prefix(strs):
     if not strs:
         return ""
 
-    # Tri des chaînes pour obtenir la plus courte en premier
+
     strs.sort()
     shortest = strs[0]
     longest = strs[-1]
 
-    # Trouver le préfixe commun entre la première et la dernière chaîne
+    
     for i in range(len(shortest)):
         if shortest[i] != longest[i]:
             return shortest[:i]
 
     return shortest
 
-def main():
-    # Entrée de l'utilisateur : une liste de chaînes de caractères
-    strings = input("Entrez les chaînes de caractères, séparées par des espaces : ").split()
-
-    # Appel de la fonction pour trouver le plus long préfixe commun
-    common_prefix = longest_common_prefix(strings)
-
-    # Affichage du résultat
-    if common_prefix:
-        print("Le plus long préfixe commun est :", common_prefix)
-    else:
-        print("Il n'y a pas de préfixe commun.")
+def main(generatedData):
+    strs = generatedData["strs"]
+    result = longest_common_prefix(strs)
+    print(result)
 
 if __name__ == "__main__":
     main()
