54bcf9476075c73fe888f4a84d8332e02fd001a1
axellllllllllllllllll <malherbeax@cy-tech.fr>
2024-05-30 14:14:20 +0200

    a

--- system/src/ui/WelcomeView.java
+++ system/src/ui/WelcomeView.java
@@ -1,13 +1,11 @@
 package ui;
 
 import Problems.ProblemManager;
-import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
